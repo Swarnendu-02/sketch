2ccdcbfd804f53a311ee2a425754d4b9717f4f61
Swarnendu-94 <60955703+Swarnendu-02@users.noreply.github.com>
2024-04-27 10:59:51 +0530

    updated

---
+++ includes/footer.php
@@ -0,0 +1,71 @@
+<footer class="container-fluid py-5">
+    <div class="container">
+        <div class="row">
+
+            <div class="col-12 col-md-6 ft-lt">
+                <img src="images/logo.png" class="mb-3">
+                <ul>
+                    <li>
+                        <p class="mb-0">Get Free Estimation</p>
+                        <a href="#" style="color: var(--red);">hello@webmaa.com</a>
+                        <p class="mb-0 mt-3">Skype</p>
+                        <a href="#" style="color: var(--red);">hello.webmaa</a>
+                    </li>
+                    <li>
+                        <p class="mb-0">Call</p>
+                        <a href="#">+91 9830990076</a>
+                        <a href="#">+91 9836987431</a>
+                    </li>
+                    <li>
+                        <p class="mb-0">
+                            <b>INDIA</b><br>
+                            Minarva Garden,<br>
+                            50, DH Road,<br>
+                            Kolkata 700104, WB,<br>
+                            India
+                        </p>
+                    </li>
+                </ul>
+            </div>
+            <div class="col-12 col-md-3 ft-lt ft-rt">
+                <b class="mb-3">Company</b>
+                <ul>
+                    <li>
+                        <a href="#">About us</a>
+                        <a href="#">Career</a>
+                        <a href="#">Blog</a>
+                        <a href="#">How to engage</a>
+                    </li>
+                    <li>
+                        <a href="#">FAQ’s</a>
+                        <a href="#">Policies</a>
+                        <a href="#">Sitemap</a>
+                        <a href="#">Contact</a>
+                    </li>
+                </ul>
+            </div>
+            <div class="col-12 col-md-3 ft-lt ft-rt">
+                <b class="mb-3">Digital Acceleration Editorial</b>
+                <p class="mb-0">Boost innovation with insights for change</p>
+                <form class="d-flex align-items-center" style="background: #E1E1E1;height: 43px;padding: 0 9px;border-radius: 5px;margin-top: 5px;">
+                    <input style="padding: 6px;background: transparent;border: none;width: calc(100% - 44px);">
+                    <button style="padding: 0;background: transparent;border: none;font-family: Poppins;font-size: 12px;font-weight: 600;line-height: 18px;text-align: left;width: 44px;">Submit</button>
+                </form>
+                <label class="containe">I agree to receive marketing communication from Webmaa
+                    <input type="checkbox" checked="checked">
+                    <span class="checkmark"></span>
+                </label>
+            </div>
+        </div>
+    </div>
+</footer>
+<div class="container-fluid py-4">
+    <div class="container">
+        <div class="row">
+            <div class="col-12 sm-ft">
+                <p>© 2024 Webmaa. All rights reserved. &nbsp;|&nbsp; <a href="#">Portfolio</a> &nbsp;|&nbsp; <a href="#">Blog</a> &nbsp;|&nbsp; <a href="#">Privacy Policy</a> &nbsp;|&nbsp; <a href="#">Sitemap</a></p>
+                <p class="text-md-right">Follow us on<a href="#"><i class="fab fa-linkedin-in"></i></a><a href="#"><i class="fab fa-dribbble"></i></a><a href="#"><i class="fab fa-github"></i></a><a href="#"><i class="fab fa-youtube"></i></a><a href="#"><i class="fab fa-instagram"></i></a><a href="#"><i class="fab fa-facebook"></i></a></p>
+            </div>
+        </div>
+    </div>
+</div>
---
+++ includes/pagesource.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml">
+
+<head>
+  <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+  <title>NEET-O-METER</title>
+  <meta name="author" content="Alvaro Trigo Lopez" />
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta name="description" content="fullPage very simple demo." />
+  <meta name="keywords" content="fullpage,jquery,demo,simple" />
+  <meta name="Resource-type" content="Document" />
+  <link rel="stylesheet" type="text/css" href="bootstrap-5/css/bootstrap.css">
+  <link rel="stylesheet" type="text/css" href="css/style.css">
+  <link rel="stylesheet" type="text/css" href="css/responcive.css">
+  <link href="css/owl.carousel.min.css?v=2" rel="stylesheet" type="text/css">
+  <link href="css/owl.theme.default.min.css?v=2" rel="stylesheet" type="text/css">
+  <link href="css/odometer.css?v=2" rel="stylesheet" type="text/css">
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.0/css/all.css" integrity="sha384-lZN37f5QGtY3VHgisS14W3ExzMWZxybE1SJSEsQp9S+oqd12jhcu+A56Ebc1zFSJ" crossorigin="anonymous">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css" />
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.css" />
+  <script src="js/jquery.min.js"></script>
+  <script src="js/owl.carousel.js"></script>
+  <script src="js/odometer.js"></script>
+  <script src="js/aos.js"></script>
+  <script src="https://unpkg.com/chart.js@2.8.0/dist/Chart.bundle.js"></script>
+  <script src="https://unpkg.com/chartjs-gauge@0.3.0/dist/chartjs-gauge.js"></script>
+
+</head>
